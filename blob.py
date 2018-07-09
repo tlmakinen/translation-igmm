2564aa4b6acb7cd9886890362a635d94ca97d5ed
Timothy makinen <tmakinen@win.igmm.cnrs.fr>
2018-07-09 17:20:18 +0200

    let's find some blobs

---
+++ blob.py
@@ -0,0 +1,7 @@
+
+from skimage.filters import gaussian
+import matplotlib.pyplot as plt
+import tifffile
+import pyqtgraph as pg
+import czifile  # for reading the microscope file
+from filtering import czi_gauss
